bac53c98aebbb8d1ba2a6fb695b08425fa3fe0c0
kmilcarz <j.milcarz@yahoo.com>
2017-12-04 19:37:33 +0100

    nav, footer, logos-slider

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,8 +16,8 @@
      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-     <script src="{{ asset('js/brandslider.css') }}"></script>
-     <script src="{{ asset('js/functions.css') }}"></script>
+     <script src="{{ asset('js/brandslider.js') }}"></script>
+     <script src="{{ asset('js/functions.js') }}"></script>
      @yield('site-scripts')
 </body>
 </html>
